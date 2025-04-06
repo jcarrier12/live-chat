b6bc3ed3b4c0464b5b5b38ca5da9da17572e41cd
jcarrier12 <75959043+jcarrier12@users.noreply.github.com>
2025-04-06 19:24:26 -0400

    Add files via upload

---
+++ live-chat-complete/server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("message:send", (msg) => {
+    io.emit("message:receive", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Live Chat Backend Running");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
